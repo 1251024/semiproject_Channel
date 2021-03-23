c5def3fc9fa898bca1493769ac9c15d51d2e34c2
1251024 <1251024@gmail.com>
2021-03-23 23:08:59 +0900

    jbo commit

---
+++ Channel/src/channel/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package channel.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/PaymentController")
+public class PaymentController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public PaymentController() {
+        super();
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
