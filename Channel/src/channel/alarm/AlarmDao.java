7aae5b63dd485bb5f6ca76a89f791787d8590aa8
jmjnssss <ssul0322@hanmail.net>
2021-04-06 10:20:24 +0900

    peh commit

--- Channel/src/channel/alarm/AlarmDao.java
+++ Channel/src/channel/alarm/AlarmDao.java
@@ -1,8 +1,15 @@
 package channel.alarm;
 
+import java.util.List;
+
+import channel.chat.ChatDto;
 
 public interface AlarmDao {
 	
 	public int selectAlarm(int num);
 
+
+	public List<ChatDto> chatAlarm(int member_num);
+	
+	public List<ChatDto> chatAlarmList(int member_num);
 }
--- Channel/src/channel/alarm/AlarmDaoImpl.java
+++ Channel/src/channel/alarm/AlarmDaoImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 
+import channel.chat.ChatDto;
+
 
 
 public class AlarmDaoImpl extends channel.db.peh.SqlMapConfig implements AlarmDao{
@@ -42,5 +44,56 @@ public class AlarmDaoImpl extends channel.db.peh.SqlMapConfig implements AlarmDa
 		return lastchat_num;
 		
 	}
+
+	@Override
+	public List<ChatDto> chatAlarm(int member_num) {
+		System.out.println("진입함");
+		SqlSession session = null;
+		List<ChatDto> list = null;
+		
+		try {
+			session = getSqlSessionFactory().openSession(true);
+			list = session.selectList("pehmapper.alarmLastnum", member_num);
+				
+			System.out.println("실행됐음");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		
+		
+		
+		return list;
+		
+	}
+
+	@Override
+	public List<ChatDto> chatAlarmList(int member_num) {
+		System.out.println("진입함");
+		SqlSession session = null;
+		List<ChatDto> list = null;
+		
+		try {
+			session = getSqlSessionFactory().openSession(true);
+			list = session.selectList("pehmapper.alarmList", member_num);
+				
+			System.out.println("실행됐음");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		
+		return list;
+		
+	}
+	
+	
+	
+	
+	
 
 }
