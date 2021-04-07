7a5f9ec4924aed017da68f47dae5ed9c975a32f5
RUCKUSJERRY <73293024+RUCKUSJERRY@users.noreply.github.com>
2021-04-07 16:40:58 +0900

    file update lyj

--- Channel/src/channel/controller/FileController.java
+++ Channel/src/channel/controller/FileController.java
@@ -1,9 +1,11 @@
 package channel.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Enumeration;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,34 +32,49 @@ public class FileController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
-		System.out.println("들어왔니??");
+
 		 // 이미지 업로드할 경로
-		String uploadPath = "C:/Workspaces/Workspace_Project/Channel/WebContent/resources/image/upload/";
+		String saveDirectory = request.getSession().getServletContext().getRealPath("resources/image/upload");
+		
+		// 파일 저장 경로
+				File file = new File(saveDirectory);
+				try {
+					if (!file.exists()) {
+						file.mkdirs();
+					}
+				} catch (Exception e2) {
+					e2.printStackTrace();
+				}
+				
+				System.out.println(saveDirectory);
+		
 	    int size = 10 * 1024 * 1024;  // 업로드 사이즈 제한 10M 이하
 		
-		String fileName = ""; // 파일명
+	    PrintWriter out = response.getWriter();
+	    
 		
-		try{
 	        // 파일업로드 및 업로드 후 파일명 가져옴
-			MultipartRequest multi = new MultipartRequest(request, uploadPath, size, "utf-8", new DefaultFileRenamePolicy());
-			Enumeration files = multi.getFileNames();
-			String file = (String)files.nextElement();
-			fileName = multi.getFilesystemName(file);
+			MultipartRequest multi = new MultipartRequest(request, saveDirectory, size, "utf-8", new DefaultFileRenamePolicy());
+			Enumeration<?> files = multi.getFileNames();
+			String formName = (String)files.nextElement();
+			String fileName = multi.getFilesystemName(formName);
 			
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		
-	    // 업로드된 경로와 파일명을 통해 이미지의 경로를 생성
-		uploadPath = "resources/image/upload/"+ fileName;
-		
-	    // 생성된 경로를 JSON 형식으로 보내주기 위한 설정
-		JSONObject jobj = new JSONObject();
-		jobj.put("url", uploadPath);
-		
-		response.setContentType("application/json"); // 데이터 타입을 json으로 설정하기 위한 세팅
-		PrintWriter out = response.getWriter();
-		out.print(jobj.toJSONString());
+			if (fileName == null) {
+				// 파일이 업로드 되지 않았을때 
+				out.print("파일 업로드 되지 않았음"); 
+			} else {
+				// 업로드된 경로와 파일명을 통해 이미지의 경로를 생성
+				String uploadPath = "resources/image/upload/" + fileName;
+				System.out.println("resources/image/upload/" + fileName);
+				
+				// 생성된 경로를 JSON 형식으로 보내주기 위한 설정
+				JSONObject jobj = new JSONObject();
+				jobj.put("url", uploadPath);
+				jobj.put("fileName", fileName);
+				
+				response.setContentType("application/json"); // 데이터 타입을 json으로 설정하기 위한 세팅
+				out.print(jobj.toJSONString());
+			}
 	}
 
 }
